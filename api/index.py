87cf03fbb28a8dd1789f94c4db3ce4cf333bc814
Felix Walton <70980912+Felix-Walton@users.noreply.github.com>
2025-07-13 22:11:49 +0100

    more input fiixes

--- api/index.py
+++ api/index.py
@@ -3,8 +3,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 from .sunsave.simulate import estimate_generation as calculate_daily_solar_generation
-from .sunsave.dispatch import run_dispatch_simulation
-from .sunsave.octopus_prices import get_current_agile_prices
+from .sunsave.dispatch import greedy_dispatch as run_dispatch_simulation
+from .sunsave.octopus_prices import agile_prices as get_current_agile_prices
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
