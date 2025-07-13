e9bbae16c3c8755fa9a0eae33b12118b7e2ae4a4
Felix Walton <70980912+Felix-Walton@users.noreply.github.com>
2025-07-13 19:56:27 +0100

    indexjs update

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
-// src/App.js
-import { useLocation } from 'react-router-dom'; // Keep useLocation if App needs it
-import SolarLink from './SolarLink';
-import ToolModal from './components/Tool';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AppWrapper from './App';    
+import reportWebVitals from './reportWebVitals';
 
-// IMPORTANT: Renamed from AppWrapper to App (or whatever you call the component that IS your app)
-// The default export should be the component that represents your main application content.
-export default function App() { // <--- Changed from AppWrapper and removed BrowserRouter
-  const location = useLocation();
-  const toolOpen  = location.pathname === '/tool';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AppWrapper />
+  </React.StrictMode>
+);
 
-  return (
-    <>
-      {/* background page is **always** mounted */}
-      <SolarLink />
-
-      {/* overlay appears only on /tool */}
-      {toolOpen && <ToolModal />}
-    </>
-  );
-}
-
+reportWebVitals();
